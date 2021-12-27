d47c240b4b4bcb76c787577a2ebb5b75f60facdf
Ram Vittal <rvvittal@amazon.com>
2021-12-27 06:12:29 -0800

    ss proxy baseline

---
+++ ssproxycdk/src/config.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -xe
+
+# Update with optional user data that will run on instance start.
+# Learn more about user-data: https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/user-data.html
+
+sudo yum install git -y
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
+node -e "console.log('Running Node.js ' + process.version)"
+mkdir ~/sagemaker-studio-proxy
+git clone https://github.com/rvvittal/sagemaker-studio-proxy-basic.git ~/sagemaker-studio-proxy
---
+++ ssproxycdk/test/ssproxycdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Ssproxycdk from '../lib/ssproxycdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ssproxycdk-stack.ts
+//test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Ssproxycdk.SsproxycdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+//});
